a4f2652e998c64106eef976224c364654a96e18c
Avi Pogrow <rpogrow@gmail.com>
2016-03-02 07:37:54 -0500

    Set up UI elements. Two Textfields, one webView, one Label

--- WebView+TextFieleds/ViewController.swift
+++ WebView+TextFieleds/ViewController.swift
@@ -4,11 +4,20 @@
 //
 //  Created by new on 3/2/16.
 //  Copyright © 2016 Avi Pogrow. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
+
+	@IBOutlet weak var nameTextField: UITextField!
+	
+	@IBOutlet weak var urlTextField: UITextField!
+	
+	
+	@IBOutlet weak var welcomeLabel: UILabel!
+
+	@IBOutlet weak var webView: UIWebView!
+
+
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
@@ -19,7 +28,10 @@ class ViewController: UIViewController {
 		super.didReceiveMemoryWarning()
 		// Dispose of any resources that can be recreated.
 	}
-
+	func updateLabel() {
+		welcomeLabel.text = "\(urlTextField.text) + \(nameTextField.text)"
+	
+	}
 
 }
 
