5d740d6b8ec4df6277344b98be37b9f017e45ed3
Avi Pogrow <rpogrow@gmail.com>
2016-03-02 08:15:38 -0500

    add delegate methods to respond to the textField invocations

--- WebView+TextFieleds/ViewController.swift
+++ WebView+TextFieleds/ViewController.swift
@@ -28,8 +28,39 @@ class ViewController: UIViewController {
 		super.didReceiveMemoryWarning()
 		// Dispose of any resources that can be recreated.
 	}
+	/******** textField delegate methods ********************/
+		// MARK: - 3 TextField Delegate methods
+	//1 This method listens for changes to the text field and asks "Can I make changes?"
+	func textField(textField: UITextField,
+		shouldChangeCharactersInRange range: NSRange,
+									  replacementString string: String) -> Bool {
+
+        // Figure out what the new text will be, if we return true
+        var newText: NSString = textField.text!
+        newText = newText.stringByReplacingCharactersInRange(range, withString: string)
+        
+		// returning true gives the text field permission to change its text
+        return true
+    }
+	
+	
+	//2 This method gets invoked if the user taps the return on the keyboard
+	func textFieldShouldReturn(textField: UITextField) -> Bool {
+		textField.resignFirstResponder()
+		
+		updateLabel()
+		
+		return true
+	}
+	//3 This method gets invoked when the user clicks inside the field
+ 	//this method clears the field
+	func textFieldDidBeginEditing(textField: UITextField) {
+		textField.text = ""
+ 	}	
+	
+	
 	func updateLabel() {
-		welcomeLabel.text = "\(urlTextField.text) + \(nameTextField.text)"
+		welcomeLabel.text = "\(urlTextField.text!) \("welcomes") \(nameTextField.text!)"
 	
 	}
 
